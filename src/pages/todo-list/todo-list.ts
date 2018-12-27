b5e6d8ae3f167f622f13460206e94e65c57d05d2
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-27 12:03:26 -0600

    initial commit

---
+++ src/pages/todo-list/todo-list.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {DataProvider} from "../../providers/data/data";
+
+/**
+ * Generated class for the TodoListPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-todo-list',
+  templateUrl: 'todo-list.html',
+})
+export class TodoListPage {
+
+  listsItems = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dataService:DataProvider) {
+    this.listsItems = this.dataService.lists;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TodoListPage');
+  }
+
+}
---
+++ src/providers/data/data.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class DataProvider {
+
+    lists = [
+        {
+            itemName: 'Milk',
+            checked: false
+        },
+        {
+            itemName: 'Cheese',
+            checked: true
+        },
+        {
+            itemName: 'Bread',
+            checked: false
+        }
+    ];
+
+    constructor() {
+        console.log('Hello DataProvider Provider');
+    }
+
+}
